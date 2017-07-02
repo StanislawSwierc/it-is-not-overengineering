951e793501ee1a4444c370cb56ee196a8321aa27
Stanislaw Swierc <stansw@microsoft.com>
2017-07-02 00:25:05 -0700

    Prepare blog post: Creating index table in DAX

---
+++ gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        markdown: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['**/*.md', '!node_modules/**', '!html/**' ],
+                        dest: 'html/'
+                    }
+                ],
+                options: {
+                    markdownOptions: {
+                        highlight: 'manual'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-markdown');
+
+    grunt.registerTask('default', ['markdown:all']);
+};
